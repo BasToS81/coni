4f2183b7544f07b93da825be9f5363a0e24e71fe
bastos81 <guillaume.ogier@gmail.com>
2014-11-09 16:21:27 +0100

    DTO Objets crees + Controller Famille

--- src/main/java/com/gor/sellphotos/SellPhotos.java
+++ src/main/java/com/gor/sellphotos/SellPhotos.java
@@ -2,9 +2,6 @@ package com.gor.sellphotos;
 
 import javax.annotation.PreDestroy;
 
-import ma.glasnost.orika.MapperFactory;
-import ma.glasnost.orika.impl.DefaultMapperFactory;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
@@ -12,9 +9,6 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
-
-import com.gor.sellphotos.dao.Famille;
-import com.gor.sellphotos.dto.FamilleDTO;
 
 /**
  * see http://info.michael-simons.eu/2014/04/15/spring-boot-as-a-backend-for-angularjs/
